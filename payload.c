5eadec1558f828ac17efa71e99fb7ee21fb9e9a3
ddening <44156662+ddening@users.noreply.github.com>
2023-04-13 10:39:41 +0200

    Copy packet and added free function for payloads

--- payload.c
+++ payload.c
@@ -5,13 +5,19 @@ payload_t* payload_create_spi(priority_t priority, device_t* device, uint8_t* da
 
     payload_t* payload = (payload_t*)malloc(sizeof(payload_t));
 
-    if (payload == NULL) {
+    uint8_t* _data = (uint8_t*)malloc(sizeof(uint8_t) * number_of_bytes);
+
+    if (payload == NULL || _data == NULL) {
         return NULL;
+    }
+
+    for (uint8_t i = 0; i < number_of_bytes; i++) {
+        _data[i] = data[i];
     }
 
     payload->priority = priority;
     payload->protocol.spi.device = device;
-    payload->protocol.spi.data = data;
+    payload->protocol.spi.data = _data;
     payload->protocol.spi.number_of_bytes = number_of_bytes;
     payload->protocol.spi.callback = callback;
     payload->protocol.spi.container = NULL;
@@ -35,4 +41,9 @@ payload_t* payload_create_i2c(priority_t priority, device_t* device, uint8_t* da
     payload->protocol.i2c.callback = callback;
 
     return payload;
+}
+
+void payload_free_spi(payload_t* payload) {
+    free(payload->protocol.spi.data);
+    free(payload);
 }
